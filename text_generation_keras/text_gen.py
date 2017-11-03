3886ecfce441f917eb4375cee9e917d2d170db69
Kirit Thadaka <kirit.thadaka@gmail.com>
2017-11-03 21:13:16 +0530

    Modified text gen script

--- text_generation_keras/text_gen.py
+++ text_generation_keras/text_gen.py
@@ -4,85 +4,84 @@ import h5py
 import os.path
 import argparse
 import numpy                    as np
-from keras.preprocessing.text   import Tokenizer, text_to_word_sequence
 from keras.models               import Sequential
 from keras.layers               import LSTM, Dense, Dropout
 from keras.utils                import np_utils
 from keras.callbacks            import ModelCheckpoint
 
-
 SEQ_LENGTH = 100
-T = Tokenizer()
-
-# def load_data(filename):
-#     # One hot encoding with words doesn't make sense because vocab is too large
-#     data        = text_to_word_sequence(open('data/' + filename, 'r').read()) 
-#     print(len(data)) # Too large
-#     T.fit_on_texts(data)
-#     encoded     = T.texts_to_matrix(data[0], mode='count')
-#     print(len(encoded[0]))
 
 def load_data(filename):
-    filename    = "data/game_of_thrones.txt"
-    raw_text    = open(filename).read()
-    raw_text    = raw_text.lower()
+    data    = open(filename).read()
+    data    = data.lower()
 
-    return raw_text
+    return data
 
 def get_data_stats(data):
     chars       = sorted(list(set(data)))
     char_to_int = dict((c, i) for i, c in enumerate(chars))
     int_to_char = dict((i, c) for i, c in enumerate(chars))
 
-    n_vocab     = len(chars)
+    vocab_size  = len(chars)
     
-    return int_to_char, char_to_int, n_vocab
+    return int_to_char, char_to_int, vocab_size
 
 def get_formatted_data(data, vocab_size, char_to_int):
 
     print(("Total Characters: ", len(data)))
-    print(("Total Vocab: ", n_vocab))
+    print(("Total Vocab: ", vocab_size))
 
     # Using numpy append is slower
-    dataX       = []
-    dataY       = []
+    list_X      = []
+    list_Y      = []
     for i in range(0, len(data) - SEQ_LENGTH, 1):
         seq_in  = data[i : i + SEQ_LENGTH]
         seq_out = data[i + SEQ_LENGTH]
-        dataX.append([char_to_int[char] for char in seq_in])
-        dataY.append(char_to_int[seq_out])
+        list_X.append([char_to_int[char] for char in seq_in])
+        list_Y.append(char_to_int[seq_out])
     
-    n_patterns  = len(dataX)
+    n_patterns  = len(list_X)
 
-    X           = np.reshape(dataX, (n_patterns, SEQ_LENGTH, 1))
-    Y           = np_utils.to_categorical(dataY)
+    X           = np.reshape(list_X, (n_patterns, SEQ_LENGTH, 1))
+    Y           = np_utils.to_categorical(list_Y)
 
     return X, Y
 
-def create_model(n_hidden_layers, input_shape, hidden_dim, n_out):
+def create_model(n_hidden_layers, input_shape, hidden_dim, n_out, **kwargs):
+    drop        = kwargs.get('drop_rate', 0.2)
+    activ       = kwargs.get('activation', 'softmax')
+    mode        = kwargs.get('mode', 'train')
     hidden_dim  = int(hidden_dim)
     model       = Sequential()
-    model.add( LSTM(hidden_dim, input_shape = (input_shape[1], input_shape[2]), return_sequences = True ) )
-    model.add( Dropout(0.2) )
+    flag        = True
+
+    if (n_hidden_layers == 0):
+        flag = False
+    
+    model.add( LSTM(hidden_dim, input_shape = (input_shape[1], input_shape[2]), return_sequences = flag ) )
+    if mode == 'train':
+        model.add( Dropout(drop) )
 
     for i in range(n_hidden_layers - 1):
         model.add( LSTM(hidden_dim, return_sequences = True) )
-        model.add( Dropout(0.2) )
+        if mode == 'train':
+            model.add( Dropout(drop) )
 
-    model.add( LSTM(hidden_dim) )
-    model.add( Dropout(0.2) )
-    model.add( Dense(n_out, activation = 'softmax') )
+    if (n_hidden_layers > 0):
+        model.add( LSTM(hidden_dim) )
+        if mode == 'train':
+            model.add( Dropout(drop) )  
 
-    # model.add(LSTM(256, input_shape=(input_shape[1], input_shape[2]), return_sequences=True))
-    # model.add(Dropout(0.2))
-    # model.add(LSTM(256))
-    # model.add(Dropout(0.2))
-    # model.add(Dense(n_out, activation='softmax'))
+    model.add( Dense(n_out, activation = activ) )
 
     return model
 
-def train(model, X, Y, n_epochs, b_size, vocab_size):    
-    model.compile(loss = 'categorical_crossentropy', optimizer = 'adam')
+def train(model, X, Y, n_epochs, b_size, vocab_size, **kwargs):    
+    loss            = kwargs.get('loss', 'categorical_crossentropy')
+    opt             = kwargs.get('optimizer', 'adam')
+    
+    model.compile(loss = loss, optimizer = opt)
+
     filepath        = "Weights/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
     checkpoint      = ModelCheckpoint(filepath, monitor = 'loss', verbose = 1, save_best_only = True, mode = 'min')
     callbacks_list  = [checkpoint]
@@ -128,18 +127,16 @@ def read_array(filename):
 
 def main():
     parser  = argparse.ArgumentParser()
-    parser.add_argument("--train", help = \
-                        "Set --train to 1 if model is to be trained or 0 if model is to be tested", \
-                        action = "store_true")
+    parser.add_argument('--mode', default = "train")
 
     args    = parser.parse_args()
 
-    filename                            = 'game_of_thrones.txt'
+    filename                            = 'data/game_of_thrones.txt'
     data                                = load_data(filename)
     ix_to_char, char_to_ix, vocab_size  = get_data_stats(data)
 
     if not os.path.exists("data/input_data.hdf5") and not os.path.exists("data/output_data.hdf5"):
-        X, Y                            = get_formatted_data(data, vocab_size, char_to_ix)
+        X, Y    = get_formatted_data(data, vocab_size, char_to_ix)
         store_array(X, "data/input_data.hdf5")
         store_array(Y, "data/output_data.hdf5")
     else:
@@ -147,13 +144,14 @@ def main():
         X = read_array("data/input_data.hdf5")
         Y = read_array("data/output_data.hdf5")
 
-    model                               = create_model(1, X.shape, 256, Y.shape[1])
+    model   = create_model(3, X.shape, 256, Y.shape[1], drop_rate = 0.3, activation = 'softmax', mode = args.mode)
 
-    if args.train:
+    if args.mode == 'train':
         print("Training")
-        train(model, X, Y, 20, 128, vocab_size)
-    else:
-        generate_text(model, X, "Weights/weights-improvement-05-1.5560.hdf5", ix_to_char, vocab_size)
+        train(model, X, Y, 100, 128, vocab_size)
+    elif args.mode == 'test':
+        print("Generating text")
+        generate_text(model, X, "Weights/weights-improvement-15-1.7141.hdf5", ix_to_char, vocab_size)
 
 if __name__ == '__main__':
     main()
